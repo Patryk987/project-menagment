239de21e0fffaba1da6ccb5a2f7e5f7976da1b8d
Patryk Pękała <patrus982@gmail.com>
2024-01-05 16:47:47 +0100

    feat: encryption system

---
+++ core/encryption-system/eas-files.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace EAS;
+
+class EncryptFile
+{
+    private int $encrypt_blocks = 10000;
+
+    private function encryptFile($source, $key, $dest)
+    {
+
+        $key = substr(sha1($key, true), 0, 16);
+        $iv = openssl_random_pseudo_bytes(16);
+
+        $error = false;
+
+        if ($fpOut = fopen($dest, 'w')) {
+
+            fwrite($fpOut, $iv);
+
+            if ($fpIn = fopen($source, 'rb')) {
+
+                while (!feof($fpIn)) {
+
+                    $plaintext = fread(
+                        $fpIn,
+                        16 * $this->encrypt_blocks
+                    );
+
+                    $ciphertext = openssl_encrypt(
+                        $plaintext,
+                        'aes-256-cbc',
+                        $key,
+                        OPENSSL_RAW_DATA,
+                        $iv
+                    );
+
+                    $iv = substr($ciphertext, 0, 16);
+                    fwrite($fpOut, $ciphertext);
+
+                }
+
+                fclose($fpIn);
+
+            } else {
+
+                $error = true;
+
+            }
+            fclose($fpOut);
+        } else {
+            $error = true;
+        }
+
+        return $error ? false : $dest;
+    }
+
+
+    private function decryptFile($source, $key, $dest)
+    {
+        $key = substr(sha1($key, true), 0, 16);
+
+        $error = false;
+        if ($fpOut = fopen($dest, 'w')) {
+            if ($fpIn = fopen($source, 'rb')) {
+
+                $iv = fread($fpIn, 16);
+                while (!feof($fpIn)) {
+
+                    $ciphertext = fread(
+                        $fpIn,
+                        16 * ($this->encrypt_blocks + 1)
+                    );
+
+                    $plaintext = openssl_decrypt(
+                        $ciphertext,
+                        'aes-256-cbc',
+                        $key,
+                        OPENSSL_RAW_DATA,
+                        $iv
+                    );
+
+                    $iv = substr($ciphertext, 0, 16);
+                    fwrite($fpOut, $plaintext);
+                }
+                fclose($fpIn);
+
+            } else {
+                $error = true;
+            }
+
+            fclose($fpOut);
+
+        } else {
+            $error = true;
+        }
+
+        return $error ? false : $dest;
+    }
+}
